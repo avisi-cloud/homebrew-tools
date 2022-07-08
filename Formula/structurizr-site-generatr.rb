0b2eba04a4d0794ed3a10d822b2aa66d4de74987
Dirk Groot <dirkgroot77@gmail.com>
2022-07-08 14:18:28 +0200

    Fix download URL and SHA256 hash

--- Formula/structurizr-site-generatr.rb
+++ Formula/structurizr-site-generatr.rb
@@ -1,8 +1,8 @@
 class StructurizrSiteGeneratr < Formula
   desc "Static site generator for architecture models created with Structrizr DSL"
   homepage "https://github.com/avisi-cloud/structurizr-site-generatr"
-  url "https://github.com/avisi-cloud/structurizr-site-generatr/archive/1.0.6.tar.gz"
-  sha256 "c488d1af988052f3037bc81a30038c93cce38e09c00e0ba38b6b4a2f9a7e51bf"
+  url "https://github.com/avisi-cloud/structurizr-site-generatr/releases/download/1.0.6/structurizr-site-generatr-1.0.6.tar.gz"
+  sha256 "00c37757b24389b79087e27b36e85f74f2bd56edeb48b2fc1094e59d60a0c8ce"
   license "Apache-2.0"
 
   depends_on "graphviz"
