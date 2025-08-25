24e91c865a052fe68fceabbab51c884f74dffdcf
idb <71566757+idebeijer@users.noreply.github.com>
2025-08-25 09:51:18 +0200

    Remove conflicts_with as its deprecated (#88)

--- Casks/acloud.rb
+++ Casks/acloud.rb
@@ -35,10 +35,6 @@ cask "acloud" do
     end
   end
 
-  conflicts_with formula: [
-      "acloud",
-    ]
-
   postflight do
     if system_command("/usr/bin/xattr", args: ["-h"]).exit_status == 0
       # replace 'acloud' with the actual binary name
